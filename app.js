226fa5d185f1be84f7fa6023bfe822fbd3232fd5
Gup Anecko <aneckogri@gmail.com>
2023-08-30 10:55:22 +0300

    fix: app.js

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', router);
-
+app.use('/api', async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  router;
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
